2d98f29da3e808ee54b4001c5c31a509dd14d2ff
benjbuch <benjamin.buchmuller@gmail.com>
2021-07-10 20:48:51 +0200

    Fix bug that causes "upper" estimate to be too low with exact fitting formula

--- R/fit_affinity.R
+++ R/fit_affinity.R
@@ -116,8 +116,8 @@
 #' @export
 fit_Kd <- function(x, formula, R0 = NaN, include_hill = FALSE,
                    limits_lower = c(-Inf, +Inf), limits_upper = c(-Inf, +Inf),
-                   limits_hill = c(-Inf, +Inf), limits_K_d = c(0, 1e3), start = "auto",
-                   ..., FUN = "minpack.lm::nlsLM") {
+                   limits_hill = c(-Inf, +Inf), limits_K_d = c(0, 1e3),
+                   start = "auto", ..., FUN = "minpack.lm::nlsLM") {
 
   RL <- formula.tools::lhs(formula)
   L0 <- formula.tools::rhs(formula)
@@ -125,7 +125,7 @@ fit_Kd <- function(x, formula, R0 = NaN, include_hill = FALSE,
   if (is.finite(R0)) {
 
     FML <- substitute(RL ~ I(lower + (upper - lower) * ((R0 + L0^hill + K_d) - sqrt(
-      (R0 + L0^hill + K_d)^2 - 4 * R0 * L0^hill)) / 2),
+      (R0 + L0^hill + K_d)^2 - 4 * R0 * L0^hill)) / (2 * R0)),
       list(R0 = R0, RL = RL, L0 = L0))
 
   } else {
